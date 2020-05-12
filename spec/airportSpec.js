9ead9350535776ed56be83bf9788d26d46f04856
Joanne Young <joanneyoung_@hotmail.com>
2020-05-12 16:47:12 +0100

    planes can land at airport, and is removed from aiport

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -14,5 +14,10 @@ describe('Airport', function(){
     airport.clearForLanding(plane);
     expect(airport.planes()).toEqual([plane]);
   });
+  it('can clear plans for takeoff', function (){
+    airport.clearForLanding(plane);
+    airport.clearForTakeOff(plane);
+    expect(airport.planes()).toEqual([]);
+  });
 });
   
